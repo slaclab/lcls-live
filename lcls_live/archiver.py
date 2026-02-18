7971b2d343d070583bee63b3ee8e4e2d82c6f557
Chris Garnier <cgarnier@slac.stanford.edu>
2026-02-18 12:05:06 -0800

    added an arg to use full timestamp instead or just 1 second resolution

--- lcls_live/archiver.py
+++ lcls_live/archiver.py
@@ -2,7 +2,7 @@
 
 import requests
 import pandas as pd
-
+import json
 
 
 def lcls_archiver_restore(pvlist, isotime='2018-08-11T10:40:00.000-07:00', verbose=True):
@@ -43,7 +43,7 @@ def lcls_archiver_restore(pvlist, isotime='2018-08-11T10:40:00.000-07:00', verbo
 def lcls_archiver_history(pvname:str,
                         start: str ='2018-08-11T10:40:00.000-07:00', 
                         end: str ='2018-08-11T11:40:00.000-07:00',
-                        verbose=True,
+                        verbose=True, full_timestamp = False,
                         raise_error: bool = True):
     """
     Get time series data from a PV name pvname,
@@ -52,9 +52,7 @@ def lcls_archiver_history(pvname:str,
     https://slacmshankar.github.io/epicsarchiver_docs/userguide.html
     
     Returns tuple: 
-        secs, vals
-    where secs is the UNIX timestamp, seconds since January 1, 1970, and vals are the values at those times.
-    
+        timestamp, vals    
     Seconds can be converted to a datetime object using:
     import datetime
     datetime.datetime.utcfromtimestamp(secs[0])
@@ -73,9 +71,21 @@ def lcls_archiver_history(pvname:str,
 
     if r.ok:
         data =  r.json()
-        secs = [x['secs'] for x in data[0]['data']]
+        #print(data)
+        #with open("output.json", "w") as f:
+        #    json.dump(data, f, indent=4)
+        if full_timestamp:
+            try:
+                timestamp = [(x['secs'] + x['nanos']*1e-9) for x in data[0]['data']]
+            except KeyError as e: 
+                print(f'Error with returned data for {pvname} : {e}')
+                print(f'Using seconds time resolution')
+                timestamp = [x['secs'] for x in data[0]['data']]
+        else:
+            timestamp = [x['secs'] for x in data[0]['data']]
+        
         vals = [x['val'] for x in data[0]['data']]
-        return secs, vals
+        return timestamp, vals
     
     msg = f"Archiver request failed for {pvname}. Response was: {r.status_code} - {r.reason}"
     if raise_error:
@@ -100,9 +110,9 @@ def lcls_archiver_history_dataframe(
     dfs = []
 
     for pv in pvs:
-        secs, vals = lcls_archiver_history(pv, **kwargs)
+        timestamp, vals= lcls_archiver_history(pv, **kwargs)
 
-        ser = pd.to_datetime(secs, unit="s")
+        ser = pd.to_datetime(timestamp, unit="s")
         df = pd.DataFrame({pv: vals}, index=ser)
         df.index.name = "time"
         df = df[~df.index.duplicated(keep="last")]
